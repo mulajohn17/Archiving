ef94a568c13a2b7d9a2f17883251e2e8bcef3589
jamydevillota@gmail.com <jamjamhonrejas@gmail.com>
2017-05-09 16:09:40 +0800

    DAMI PALANG NA

---
+++ FINAL/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>St. Elizabeth</title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<body>
+<div id="head">
+<img src="icon/sehilogo-small.PNG">
+<div id = "menu">
+	<ul>
+			<a href = "prof.php" target="main"><li class = "active" id = "choices" >Profiling</li></a>
+			<a href = "list.php" target="main"><li id = "choices">Employee List</li></a>
+			<a href = "pending.php" target="main"><li id = "choices">Pending</li></a>
+			<a href = "act.php" target="main"><li id = "choices">Activity Log</li></a>
+			<a href = "manage/dash.php" target="main"><li id = "choices">Manage</li></a>
+			<a href = "report/index.php" target="main"><li id = "choices">REPORT</li></a>
+			<a href = "out.php"><li id = "choices">Log Out</li></a>
+
+	</ul>
+	</div>
+</div>
+</body>
+</html>
