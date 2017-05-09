e0127ec90673f7daf60b6e312a138f44ce58655c
jamydevillota@gmail.com <jamjamhonrejas@gmail.com>
2017-05-09 14:24:12 +0800

    NEW FOLDER

--- Codes/index.php
+++ Codes/index.php
@@ -1,1 +1,10 @@
-jam here
+<!DOCTYPE html>
+<html>
+<head>
+	<title>St. Elizabeth</title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<body>
+<div id="head">Jam</div>
+</body>
+</html>
